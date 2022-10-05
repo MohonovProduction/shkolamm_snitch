5d31e014f06e21ba4474c4a7365ad545a8817e74
Mihail <mohonovproduction@yandex.ru>
2022-10-05 11:53:26 +0300

    move out http req

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const devMode = true
 
 const configs = {
     dev: {
-        uri: 'http://uchebarutualfa',
+        uri: 'https://uchebarutualfa',
     },
     prod: {
        uri: 'https://shmmhost.ru/mohonov/uchebaru'
@@ -16,31 +16,33 @@ const config = devMode ? configs.dev : configs.prod
 
 let lastDate = formatDate(Date.now())
 
-http.get(`${config.uri}/?dateFrom=${lastDate}`, res => {
-    console.log(res)
-    console.log(`Request: ${config.uri}/?dateFrom=${lastDate}`)
-})
+sendRequest()
 
 console.log(lastDate)
 lastDate = formatDate(Date.now())
 
 setInterval(() => {
-    http.get(`${config.uri}/?dateFrom=${lastDate}`, res => {
-        console.log(res)
-    })
+    sendRequest()
 
     console.log(lastDate)
     lastDate = formatDate(Date.now())
 },  5 * 60  * 1000)
 
+function sendRequest() {
+    https.get(`${config.uri}/?dateFrom=${lastDate}`, res => {
+        console.log(res)
+        console.log(`Request: ${config.uri}/?dateFrom=${lastDate}`)
+    })
+}
+
 function formatDate(timestamp) {
-    const date = new Date()
+    const date = new Date(timestamp)
     const year = date.getFullYear()
     const month = (date.getMonth() + 1 < 10) ? '0' + date.getMonth() + 1 : date.getMonth() + 1
     const day = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
+    const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours()
+    const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()
+    const seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds()
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 }
